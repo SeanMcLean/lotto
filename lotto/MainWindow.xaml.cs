1b86334f01fdf4fc6507a2098357f12826f5387e
Sean <l00104205@student.lyit.ie>
2017-10-06 16:37:08 +0100

    First Push of random lotto generator

--- lotto/MainWindow.xaml.cs
+++ lotto/MainWindow.xaml.cs
@@ -24,5 +24,59 @@ namespace lotto
         {
             InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            int Min = 1;
+            int Max = 52;
+        
+            //initials new random object
+            Random randomNumbers = new Random();
+
+            int lottoNumbers = randomNumbers.Next(1, 52);
+            int lottoBonusNumber = randomNumbers.Next(1, 52);
+
+            int[] lottoArray = new int[7];
+
+            for (int arrayPos = 0; arrayPos < lottoArray.Length; arrayPos++)
+            {
+                lottoArray[arrayPos] = randomNumbers.Next(Min, Max);
+            }
+            tbx0.Text = Convert.ToString(lottoArray.GetValue(0));
+            tbx1.Text = Convert.ToString(lottoArray.GetValue(1));
+            tbx2.Text = Convert.ToString(lottoArray.GetValue(2));
+            tbx3.Text = Convert.ToString(lottoArray.GetValue(3));
+            tbx4.Text = Convert.ToString(lottoArray.GetValue(4));
+            tbx5.Text = Convert.ToString(lottoArray.GetValue(5));
+            tbx6.Text = Convert.ToString(lottoArray.GetValue(6));
+            tbxBonus.Text = Convert.ToString(lottoBonusNumber);
+        }
+
+        private void btnUK_Click(object sender, RoutedEventArgs e)
+        {
+            int Min = 1;
+            int Max = 58;
+
+            //initials new random object
+            Random randomNumbers = new Random();
+
+            int lottoNumbers = randomNumbers.Next(1, 52);
+            int lottoBonusNumber = randomNumbers.Next(1, 52);
+            int[] lottoArray = new int[7];
+
+            for (int arrayPos = 0; arrayPos < lottoArray.Length; arrayPos++)
+            {
+                lottoArray[arrayPos] = randomNumbers.Next(Min, Max);
+            }
+            tbx0.Text = Convert.ToString(lottoArray.GetValue(0));
+            tbx1.Text = Convert.ToString(lottoArray.GetValue(1));
+            tbx2.Text = Convert.ToString(lottoArray.GetValue(2));
+            tbx3.Text = Convert.ToString(lottoArray.GetValue(3));
+            tbx4.Text = Convert.ToString(lottoArray.GetValue(4));
+            tbx5.Text = Convert.ToString(lottoArray.GetValue(5));
+            tbx6.Text = Convert.ToString(lottoArray.GetValue(6));
+            tbxBonus.Text = Convert.ToString(lottoBonusNumber);
+        }
     }
 }
+
